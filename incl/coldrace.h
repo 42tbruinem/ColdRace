74ade279bf54c31954cf1725e20ef5a65dd9ed5a
Thijs Bruineman <t_b@live.nl>
2021-12-14 14:51:13 +0100

    feat: added storage mode

--- incl/coldrace.h
+++ incl/coldrace.h
@@ -1,5 +1,3 @@
-#ifndef COLDRACE_H
-# define COLDRACE_H
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        ::::::::            */
@@ -8,10 +6,13 @@
 /*   By: tbruinem <tbruinem@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/12/14 14:16:50 by tbruinem      #+#    #+#                 */
-/*   Updated: 2021/12/14 14:17:08 by tbruinem      ########   odam.nl         */
+/*   Updated: 2021/12/14 14:40:31 by tbruinem      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stddef.h>
 #include "libtrie.h"
-#endif
+#include <unistd.h>
+#include <fcntl.h>
+
+
--- src/main.c
+++ src/main.c
@@ -6,13 +6,44 @@
 /*   By: tbruinem <tbruinem@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/12/14 14:15:01 by tbruinem      #+#    #+#                 */
-/*   Updated: 2021/12/14 14:37:00 by tbruinem      ########   odam.nl         */
+/*   Updated: 2021/12/14 14:49:55 by tbruinem      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "coldrace.h"
+#include <string.h>
+#include <stdbool.h>
+
+#define KEY true
+#define VALUE false
 
 int main(void) {
 	Trie*	storage = trie_new();
 	char*	line = NULL;
+	bool	type = KEY;
+	char	*key = NULL;
+
+	// Storage mode
+	do {
+		// Break if we encounter an empty line, end of storage mode
+		if (!strlen(line)) {
+			break;
+		}
+		// Store the line as the 'key'
+		if (type == KEY) {
+			free(key);
+			key = line;
+		}
+		// Insert the value at 'key'
+		else {
+			trie_insert(&storage, key, line);
+			free(line);
+		}
+		type = !type;
+	}
+	while (get_next_line(STDIN_FILENO, &line) != -1);
+
+	// Search mode
+
+	return 0;
 }
