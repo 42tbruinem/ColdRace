8ed9700cbd8a03b72bffa3f66df99fb3fd5b51e4
Tishj <t_b@live.nl>
2021-12-15 12:05:04 +0100

    feat: updated generate_inputs

--- generate_inputs.py
+++ generate_inputs.py
@@ -22,27 +22,42 @@ def generate_pair():
 	value = "VALUE - " + generate_single(args.k)
 	return (key,value)
 
-pairs = []
+storage = dict()
 for _ in range(args.s):
-	pairs.append(generate_pair())
+	key, value = generate_pair()
+	storage[key] = value
 
 searches = []
 valid = []
+keys = list(storage.keys())
 for _ in range(args.t):
 	if len(valid) < args.v and randrange(0,args.t) % 2 == 0:
 		# remaining_valid_keys = [x for x in pairs if x[0] not in valid]
-		remaining_valid_keys = pairs
+		remaining_valid_keys = keys
 		valid_key = choices(remaining_valid_keys)[0]
-		valid.append(valid_key[0])
+		valid.append(valid_key)
 		searches.append(valid_key)
 	else:
-		searches.append(generate_pair())
+		searches.append(generate_single(args.k))
 
 # Output result
 
-for pair in pairs:
-	print(pair[0], pair[1], sep="\n")
-print("")
+input_content = ""
+output_content = ""
+
+for key,value in storage.items():
+	input_content += key + "\n"
+	input_content += value + "\n"
+input_content += "\n"
 for search in searches:
-	print(search[0])
-print("")
+	input_content += search + "\n"
+	if search in storage:
+		output_content += storage[search] + "\n"
+	else:
+		output_content += search + ": Not Found.\n"
+input_content += "\n"
+
+with open(f"./inputs/{args.s}_{args.k}_{args.t}_{args.v}.input", 'w') as f:
+	f.write(input_content)
+with open(f"./inputs/{args.s}_{args.k}_{args.t}_{args.v}.output", 'w') as f:
+	f.write(output_content)
